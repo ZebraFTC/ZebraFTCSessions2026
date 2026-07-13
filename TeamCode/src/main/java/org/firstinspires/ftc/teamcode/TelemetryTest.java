77b94009eff0899df2503622484d1a6aad0c7b14
ZebraFTC <zebracompetitionteams@gmail.com>
2026-07-13 17:20:34 -0400

    adjust gradle settings, telemetry orks in panels

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TelemetryTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TelemetryTest.java
@@ -1,20 +1,26 @@
 package org.firstinspires.ftc.teamcode;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @TeleOp
 public class TelemetryTest extends OpMode{
+    private TelemetryManager panelsTelemetry;
 
     @Override
     public void init() {
-
+        panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
+        panelsTelemetry.addData("OpMode", "Initialised");
+        panelsTelemetry.update(telemetry);
     }
 
     @Override
     public void loop() {
+        panelsTelemetry.addData("OpMode", "started");
+        panelsTelemetry.update(telemetry);
 
     }
 }
