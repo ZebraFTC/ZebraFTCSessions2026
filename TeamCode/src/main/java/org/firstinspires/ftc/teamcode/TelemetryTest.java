3075a5b84353bb169aef7be958e6e92954325b62
ZebraFTC <zebracompetitionteams@gmail.com>
2026-07-13 16:49:18 -0400

    fix gradle settings so synce builds without errors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TelemetryTest.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+@TeleOp
+public class TelemetryTest extends OpMode{
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
